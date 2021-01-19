e0d7c7d5358c053d6bc05da04196113d4320de5e
Factis <53155715qwerty>
2021-01-19 16:27:18 +0800

    added some base functions and properties to PicturesViewModel

--- app/src/main/java/ru/bimlibik/pictureswitcher/PicturesViewModel.kt
+++ app/src/main/java/ru/bimlibik/pictureswitcher/PicturesViewModel.kt
@@ -1,6 +1,51 @@
 package ru.bimlibik.pictureswitcher
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
 
 class PicturesViewModel : ViewModel() {
+
+    private val loadedPictures = mutableListOf<Picture>()
+
+    private val _forceUpdate = MutableLiveData(false)
+    private val _category = MutableLiveData<String>()
+    private val _page = MutableLiveData<Int>()
+
+    private val _triggers =
+        MediatorLiveData<Triple<Boolean?, String?, Int?>>().apply {
+            addSource(_forceUpdate) { value = Triple(it, _category.value, _page.value) }
+            addSource(_category) { value = Triple(_forceUpdate.value, it, _page.value) }
+            addSource(_page) { value = Triple(_forceUpdate.value, _category.value, it) }
+        }
+
+    private val _pictures: LiveData<List<Picture>> = _triggers.switchMap { triple ->
+        val forceUpdate: Boolean = triple.first ?: false
+        val category: String = triple.second ?: DEFAULT_CATEGORY
+        val page: Int = triple.third ?: DEFAULT_PAGE
+        loadPictures(category, page)
+    }
+
+    val pictures: LiveData<List<Picture>> = _pictures
+
+    val empty: LiveData<Boolean> = Transformations.map(_pictures) {
+        it == null || it.isEmpty()
+    }
+
+    private val _showScrim = MutableLiveData(false)
+    val showScrim: LiveData<Boolean> = _showScrim
+
+
+
+    private fun loadPictures(query: String, page: Int): LiveData<List<Picture>> {
+        val result = MutableLiveData<List<Picture>>()
+        viewModelScope.launch {
+//            TODO("Load pictures from network")
+            result.value = emptyList()
+        }
+        return result
+    }
 }
+
+private const val DEFAULT_CATEGORY = ""
+private const val DEFAULT_PAGE = 1
+private const val MAX_PAGE = 25
