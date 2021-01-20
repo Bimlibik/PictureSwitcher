be6b32b1bb786a7a56940926c931a45167361e44
Factis <53155715qwerty>
2021-01-20 18:21:17 +0800

    added koin module for DI

---
+++ app/src/main/java/ru/bimlibik/pictureswitcher/AppModule.kt
@@ -0,0 +1,15 @@
+package ru.bimlibik.pictureswitcher
+
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import ru.bimlibik.pictureswitcher.data.IPicturesRepository
+import ru.bimlibik.pictureswitcher.data.PicturesDataSource
+import ru.bimlibik.pictureswitcher.data.PicturesRepository
+import ru.bimlibik.pictureswitcher.data.remote.PicturesRemoteDataSource
+
+val appModule = module {
+
+    single<PicturesDataSource> { PicturesRemoteDataSource() }
+    single<IPicturesRepository> { PicturesRepository(get()) }
+    viewModel { PicturesViewModel(get()) }
+}
