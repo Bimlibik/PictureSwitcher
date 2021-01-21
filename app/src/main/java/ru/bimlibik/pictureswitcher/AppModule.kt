36010c7818390449d481c9e18554d4ba9b58f918
Factis <53155715qwerty>
2021-01-21 19:38:03 +0800

    Added PicturesViewModel

--- app/src/main/java/ru/bimlibik/pictureswitcher/AppModule.kt
+++ app/src/main/java/ru/bimlibik/pictureswitcher/AppModule.kt
@@ -6,6 +6,7 @@ import ru.bimlibik.pictureswitcher.data.IPicturesRepository
 import ru.bimlibik.pictureswitcher.data.PicturesDataSource
 import ru.bimlibik.pictureswitcher.data.PicturesRepository
 import ru.bimlibik.pictureswitcher.data.remote.PicturesRemoteDataSource
+import ru.bimlibik.pictureswitcher.ui.picture_detail.PictureDetailViewModel
 import ru.bimlibik.pictureswitcher.ui.pictures.PicturesViewModel
 
 val appModule = module {
@@ -13,4 +14,5 @@ val appModule = module {
     single<PicturesDataSource> { PicturesRemoteDataSource() }
     single<IPicturesRepository> { PicturesRepository(get()) }
     viewModel { PicturesViewModel(get()) }
+    viewModel { PictureDetailViewModel() }
 }
