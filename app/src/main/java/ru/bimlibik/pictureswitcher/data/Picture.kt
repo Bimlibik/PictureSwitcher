cdf44ce84c083157487578fa253aaf70c14cf9b6
Factis <53155715qwerty>
2021-01-21 19:43:35 +0800

    modified to parcelable

--- app/src/main/java/ru/bimlibik/pictureswitcher/data/Picture.kt
+++ app/src/main/java/ru/bimlibik/pictureswitcher/data/Picture.kt
@@ -1,16 +1,20 @@
 package ru.bimlibik.pictureswitcher.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Parcelize
 data class Picture(
     @SerialName("id")
-    val id: String,
+    val id: @RawValue String,
 
     @SerialName("urls")
-    val urls: PictureUrl,
+    val urls: @RawValue PictureUrl,
 
     @SerialName("user")
-    val author: Author
-)
+    val author: @RawValue Author
+) : Parcelable
