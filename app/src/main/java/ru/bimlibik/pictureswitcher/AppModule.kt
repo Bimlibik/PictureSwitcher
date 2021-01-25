cc72308a0b1de15f75d3e5cbae64159f45c1e442
Factis <53155715qwerty>
2021-01-25 14:20:06 +0800

    added local data source to koin app module

--- app/src/main/java/ru/bimlibik/pictureswitcher/AppModule.kt
+++ app/src/main/java/ru/bimlibik/pictureswitcher/AppModule.kt
@@ -5,14 +5,16 @@ import org.koin.dsl.module
 import ru.bimlibik.pictureswitcher.data.IPicturesRepository
 import ru.bimlibik.pictureswitcher.data.PicturesDataSource
 import ru.bimlibik.pictureswitcher.data.PicturesRepository
+import ru.bimlibik.pictureswitcher.data.local.PicturesLocalDataSource
 import ru.bimlibik.pictureswitcher.data.remote.PicturesRemoteDataSource
 import ru.bimlibik.pictureswitcher.ui.picture_detail.PictureDetailViewModel
 import ru.bimlibik.pictureswitcher.ui.pictures.PicturesViewModel
 
 val appModule = module {
 
-    single<PicturesDataSource> { PicturesRemoteDataSource() }
-    single<IPicturesRepository> { PicturesRepository(get()) }
+    single<PicturesDataSource.Remote> { PicturesRemoteDataSource() }
+    single<PicturesDataSource.Local> { PicturesLocalDataSource() }
+    single<IPicturesRepository> { PicturesRepository(get(), get()) }
     viewModel { PicturesViewModel(get()) }
-    viewModel { PictureDetailViewModel() }
+    viewModel { PictureDetailViewModel(get()) }
 }
