3774f2043ca7a33d671e573d688b400ef09bc12d
Factis <53155715qwerty>
2021-01-25 14:30:33 +0800

    added local data source

--- app/src/main/java/ru/bimlibik/pictureswitcher/data/PicturesRepository.kt
+++ app/src/main/java/ru/bimlibik/pictureswitcher/data/PicturesRepository.kt
@@ -1,10 +1,27 @@
 package ru.bimlibik.pictureswitcher.data
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class PicturesRepository(
-    private val picturesRemoteDataSource: PicturesDataSource
+    private val picturesRemoteDataSource: PicturesDataSource.Remote,
+    private val picturesLocalDataSource: PicturesDataSource.Local,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : IPicturesRepository {
 
-    override suspend fun getPictures(query: String?, page: Int): Result<List<Picture>> {
+    override suspend fun getPictures(
+        query: String?, page: Int, fromFavorites: Boolean
+    ): Result<List<Picture>> {
+        if (fromFavorites) {
+            return picturesLocalDataSource.getFavoritePictures()
+        }
+
         return picturesRemoteDataSource.getPictures(query, page)
     }
+
+    override suspend fun updateFavorite(picture: Picture): Boolean = withContext(ioDispatcher) {
+        picturesLocalDataSource.updateFavorite(picture)
+    }
+
 }
