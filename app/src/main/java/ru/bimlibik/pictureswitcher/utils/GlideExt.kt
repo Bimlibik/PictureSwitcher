ce0d58a8f361f5a35b2e60d1470f3d4bcd3e0011
Factis <53155715qwerty>
2021-02-03 18:35:51 +0800

    when a picture is loaded from the network, using the Palette library, a color is determined that will be used to create a gradient in the toolbar

--- app/src/main/java/ru/bimlibik/pictureswitcher/utils/GlideExt.kt
+++ app/src/main/java/ru/bimlibik/pictureswitcher/utils/GlideExt.kt
@@ -4,8 +4,14 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.util.Log
 import android.widget.ImageView
-import com.bumptech.glide.Glide
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import ru.bimlibik.pictureswitcher.GlideApp
 import ru.bimlibik.pictureswitcher.R
+import ru.bimlibik.pictureswitcher.data.Picture
 import java.io.IOException
 
 private const val TAG = "GlideExt"
@@ -14,7 +20,7 @@ fun Context.getBitmap(url: String?): Bitmap? {
     if (url == null) return null
     val displayMetrics = this.resources.displayMetrics
     return try {
-        Glide.with(this)
+        GlideApp.with(this)
             .asBitmap()
             .load(url)
             .centerCrop()
@@ -26,22 +32,66 @@ fun Context.getBitmap(url: String?): Bitmap? {
     }
 }
 
-fun ImageView.setSmallPicture(url: String) {
+fun ImageView.setSmallPicture(picture: Picture) {
     val displayMetrics = this.context.resources.displayMetrics
     val width = displayMetrics.widthPixels / 2
     val height = this.height
-    Glide.with(this.context)
-        .load(url)
+    GlideApp.with(this.context)
+        .asBitmap()
+        .load(picture.urls?.small)
         .placeholder(R.drawable.ic_image)
         .centerCrop()
         .override(width, height)
+        .thumbnail(0.2f)
+        .listener(getRequestListener(picture))
         .into(this)
 }
 
 fun ImageView.setPreview(url: String) {
-    Glide.with(this.context)
+    GlideApp.with(this)
         .load(url)
         .centerCrop()
         .placeholder(R.drawable.ic_image)
+        .thumbnail(0.2f)
         .into(this)
+}
+
+
+/**
+ * The same function is used when loading images from Realm.
+ * Since Realm uses LiveData, when trying to change the [color] field, an error occurs:
+ * "Cannot modify managed objects outside of a write transaction".
+ * Therefore, a try/catch block is used here.
+ * If an error was caught, it means that the given [picture] is already in the database and
+ * the [color] value has already been calculated for it.
+ */
+private fun getRequestListener(picture: Picture) = object : RequestListener<Bitmap> {
+
+    override fun onLoadFailed(
+        e: GlideException?,
+        model: Any?,
+        target: Target<Bitmap>?,
+        isFirstResource: Boolean
+    ): Boolean {
+        Log.i(TAG, "Error while loading picture^ $e")
+        return false
+    }
+
+    override fun onResourceReady(
+        resource: Bitmap?,
+        model: Any?,
+        target: Target<Bitmap>?,
+        dataSource: DataSource?,
+        isFirstResource: Boolean
+    ): Boolean {
+        try {
+            if (resource != null) {
+                val palette: Palette = Palette.from(resource).generate()
+                picture.color = palette.darkMutedSwatch?.rgb ?: android.R.color.transparent
+            }
+        } catch (e: IllegalStateException) {
+            Log.i(TAG, "Picture color already exists.")
+        }
+        return false
+    }
 }
